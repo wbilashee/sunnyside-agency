a66fb3bad078874c0bc9d23992434af5284d5cb1
Habiba Akter Bilashee <wbilashee@gmail.com>
2022-12-28 11:01:18 +0600

    header done

---
+++ app/js/app.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const fadeItems = document.querySelectorAll(".has-fade");
+const headerToggle = document.querySelector(".header__toggle");
+
+headerToggle.addEventListener("click", function () {
+    if (header.classList.contains("open")) { // Close Hamburger Menu
+        body.classList.remove("noscroll");
+        header.classList.remove("open");
+        fadeItems.forEach(function (element) {
+            element.classList.remove("fade-in");
+            element.classList.add("fade-out");
+        });
+    }
+    else { // Open Hamburger Menu
+        body.classList.add("noscroll");
+        header.classList.add("open");
+        fadeItems.forEach(function (element) {
+            element.classList.remove("fade-out");
+            element.classList.add("fade-in");
+        });
+    }
+});
